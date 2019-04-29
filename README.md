08580379b3d316d2231e62cc33cebcad206c9419
Alex Caloggero <a.calog@protonmail.com>
2019-04-29 16:09:39 -0400

    Updated README; added public endpoint methods

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 # Python Binance API
 By: *Alex Caloggero*
 
-Python wrapper to work with the Binance[https://www.binance.com/en] API. 
+> Python wrapper to work with the [Binance](https://www.binance.com/en) API.
+
+----
+## General Information
+* Pass string queries in the form: {key: value, key: value, ...}.
+* Methods prefixed with 'get_' return a JSON object.
+* No authentication required for public requests.
+* Be mindful of API limits.
+* For detailed information on Binance's REST API visit [here](https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md).
+
+----
+## Initialize
+#### Public API Endpoints
+
+    client = Client()
+    client.ping()
+    b'{}'
+
+----
+## Methods for public Endpoints
+* server_time()
+* ping()
+* exchange_info()
+* get_order_book(symbol, limit)
+* get_average_price(symbol)
+* get_trades_list(symbol, limit)
+* get_historical_trades(symbol, limit, fromId)
+* get_aggregate_trades(symbol, fromId, startTime, endTime, limit)
+* get_candlesticks(symbol, interval, startTime, endTime, limit)
+* get_24_hr_price(symbol)
+* get_price_ticker(symbol)
+* get_book_ticker(symbol)
--- client.py
+++ client.py
@@ -4,9 +4,13 @@ import hmac
 
 class Client(object):
 
-    API_URL = 'https://api.binance.com'
+    API_URL = 'https://api.binance.com/api/'
 
-    def __init__(self, api_key, api_secret, request_params=None):
+    PUBLIC = 'v1'
+
+    PRIVATE = 'v3'
+
+    def __init__(self, api_key=None, api_secret=None):
         self.API_KEY = api_key
         self.API_SECRET = api_secret
         self.session = self._init_session()
@@ -18,22 +22,59 @@ class Client(object):
                                 'X-MBX-APIKEY': self.API_KEY})
         return session
 
-    def get_server_time(self):
-        response = self.session.get(self.API_URL + '/api/v1/time')
+    def server_time(self):
+        response = self.session.get(self.API_URL + self.PUBLIC + '/time')
         print(response.content)
 
     def ping(self):
-        response = self.session.get(self.API_URL + '/api/v1/ping')
+        response = self.session.get(self.API_URL + self.PUBLIC + '/ping')
         print(response.content)
 
-    def get_exchange_info(self):
-        response = self.session.get(self.API_URL + '/api/v1/exchangeInfo')
+    def exchange_info(self):
+        response = self.session.get(self.API_URL + self.PUBLIC + '/exchangeInfo')
         print(response.content)
 
     def get_order_book(self, request_params):
-        response = self.session.get(self.API_URL + '/api/v1/depth', params=request_params)
-        print(response.content)
+        response = self.session.get(self.API_URL + self.PUBLIC + '/depth', params=request_params)
+        return response.json()
 
-    def average_price(self, request_params):
-        response = self.session.get(self.API_URL + '/api/v3/avgPrice', params=request_params)
-        print(response.content)
+    def get_average_price(self, request_params):
+        response = self.session.get(self.API_URL + self.PRIVATE + '/avgPrice', params=request_params)
+        return response.json()
+
+    def get_trades_list(self, request_params):
+        response = self.session.get(self.API_URL + self.PUBLIC + '/trades', params=request_params)
+        return response.json()
+
+    def get_historical_trades(self, request_params):
+        response = self.session.get(self.API_URL + self.PUBLIC + '/historicalTrades', params=request_params)
+        return response.json()
+
+    def get_aggregate_trades(self, request_params):
+        response = self.session.get(self.API_URL + self.PUBLIC + '/aggTrades', params=request_params)
+        return response.json()
+
+    def get_candlesticks(self, request_params):
+        response = self.session.get(self.API_URL + self.PUBLIC + '/klines', params=request_params)
+        return response.json()
+
+    def get_24_hr_price(self, request_params):
+        try:
+            response = self.session.get(self.API_URL + self.PUBLIC + '/ticker/24hr', params=request_params)
+        except:
+            print('Error. There was a problem with your request. Try again.')
+        return response.json()
+
+    def get_price_ticker(self, request_params):
+        try:
+            response = self.session.get(self.API_URL + self.PRIVATE + '/ticker/price', params=request_params)
+        except:
+            print('Error. There was a problem with your request. Try again.')
+        return response.json()
+
+    def get_book_ticker(self, request_params):
+        try:
+            response = self.session.get(self.API_URL + self.PRIVATE + '/ticker/bookTicker', params=request_params)
+            return response.json()
+        except:
+            print('Error. There was a problem with your request. Try again.')
